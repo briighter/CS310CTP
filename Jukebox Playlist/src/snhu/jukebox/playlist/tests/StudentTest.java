3737c81b5fcb3d821654be0f6431ba75564bbc1f
williamsleischner <william.leischner@snhu.edu>
2019-07-29 13:00:18 -0600

    First commit. Added my name William Scott Leischner to the StudenList.java file and added test case for my name in StudentTest.java

--- Jukebox Playlist/src/snhu/jukebox/playlist/tests/StudentTest.java
+++ Jukebox Playlist/src/snhu/jukebox/playlist/tests/StudentTest.java
@@ -29,7 +29,13 @@ public class StudentTest {
 		studentNames = studentList.getStudentsNames();									//populate the studentNames list with the actual values in the StudentsList object
 		assertEquals("TestStudent2Name", studentNames.get(1));							//test case to see if the second value contains the name we expect
 	}
-	
+	@Test
+	public void testGetStudentNameList3() {
+		List<String> studentNames = new ArrayList<String>();							//create variable for student list of names
+		StudentList studentList = new StudentList();									//instantiate the StudentList object so we can access it's methods and properties
+		studentNames = studentList.getStudentsNames();									//populate the studentNames list with the actual values in the StudentsList object
+		assertEquals("William Scott Leischner", studentNames.get(2));							//test case to see if the second value contains the name we expect
+	}
 	//Module 5 - Add your unit test case here to check for your name after you have added it to the StudentList
 	
 	
