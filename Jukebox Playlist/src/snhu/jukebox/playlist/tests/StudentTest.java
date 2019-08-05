177ea58251cb5538f28743aafbd5470a92de3f0b
John deLuccia <john.deluccia@snhu.edu>
2019-08-04 22:48:04 -0400

    Added myself to student List.
    Created test for my name.

--- Jukebox Playlist/src/snhu/jukebox/playlist/tests/StudentTest.java
+++ Jukebox Playlist/src/snhu/jukebox/playlist/tests/StudentTest.java
@@ -30,6 +30,14 @@ public class StudentTest {
 		assertEquals("TestStudent2Name", studentNames.get(1));							//test case to see if the second value contains the name we expect
 	}
 	
+	@Test
+	public void testGetJohnDeluccia() {
+		List<String> studentNames = new ArrayList<String>();							//create variable for student list of names
+		StudentList studentList = new StudentList();									//instantiate the StudentList object so we can access it's methods and properties
+		studentNames = studentList.getStudentsNames();									//populate the studentNames list with the actual values in the StudentsList object
+		assertEquals("JohnDeluccia", studentNames.get(2));							//test case to see if the third value contains the name we expect (John DeLuccia
+	}
+	
 	//Module 5 - Add your unit test case here to check for your name after you have added it to the StudentList
 	
 	
