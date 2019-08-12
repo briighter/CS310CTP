4ea991b2b987f0aa167e2abedc748408bdf0edc7
elizabeth.tay_snhu <elizabeth.tay_snhu@NV-SNHU4-RDS5.apporto.com>
2019-08-11 23:46:37 -0400

    Added ElizabethTaylor_Playlist
    added Taylor Swift and Cranberries to list

---
+++ Dr_Manning-cs310-j6420-jukebox-playlist-68707b6e0419/Jukebox Playlist/src/snhu/student/playlists/ElizabethTaylor_Playlist.java
@@ -0,0 +1,34 @@
+package snhu.student.playlists;
+
+import snhu.jukebox.playlist.PlayableSong;
+import snhu.jukebox.playlist.Song;
+import music.artist.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ElizabethTaylor_Playlist {
+    
+	public LinkedList<PlayableSong> StudentPlaylist(){
+	
+	LinkedList<PlayableSong> playlist = new LinkedList<PlayableSong>();
+	ArrayList<Song> taylorSwiftsTracks = new ArrayList<Song>();
+    TaylorSwift taylorSwift = new TaylorSwift();
+	
+    taylorSwiftsTracks = taylorSwift.getTaylorSwiftSongs();
+	
+	playlist.add(taylorSwiftsTracks.get(0));
+	playlist.add(taylorSwiftsTracks.get(1));
+	playlist.add(taylorSwiftsTracks.get(2));
+	
+	
+    TheCranberries theCranberriesBand = new TheCranberries();
+	ArrayList<Song> theCranberriesTracks = new ArrayList<Song>();
+    theCranberriesTracks = theCranberriesBand.getCranberriesSongs();
+	
+	playlist.add(theCranberriesTracks.get(0));
+	playlist.add(theCranberriesTracks.get(1));
+	
+	
+    return playlist;
+	}
+}
