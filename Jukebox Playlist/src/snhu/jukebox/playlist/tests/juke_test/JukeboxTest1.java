72a24f863599674ef6e06e717c985b8c2f1ed3a9
Charetta Frierson <charetta.frierson@snhu.edu>
2019-08-13 23:11:31 -0400

    Tested jukebox and playlists

---
+++ Jukebox Playlist/src/snhu/jukebox/playlist/tests/juke_test/JukeboxTest1.java
@@ -0,0 +1,29 @@
+package snhu.jukebox.playlist.tests.juke_test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JukeboxTest1 {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testTestGetBeatlesAlbumSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestGetImagineDragonsAlbumSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestGetAdelesAlbumSize() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Jukebox Playlist/src/snhu/student/playlists/TestStudent2_PlaylistTest.java
@@ -0,0 +1,19 @@
+package snhu.student.playlists;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestStudent2_PlaylistTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testStudentPlaylist() {
+		fail("Not yet implemented");
+	}
+
+}
