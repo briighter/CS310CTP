aa07a4ec1fd7c2c1c09acf859d99d86cdd84c12b
John deLuccia <john.deluccia@snhu.edu>
2019-08-16 19:52:12 -0400

    Module Six fetch from upstream

--- Jukebox Playlist/src/music/artist/TheBand.java
+++ Jukebox Playlist/src/music/artist/TheBand.java
@@ -16,7 +16,7 @@ public class TheBand {
     	 albumTracks = new ArrayList<Song>();                                   //Instantiate the album so we can populate it below
     	 Song track1 = new Song("When I paint My Masterpiece", "The Band");     //Create a song
          Song track2 = new Song("Long Black Veil", "The Band");
-         Song track3 = new Song("Ophelia", "The Band");																		//Create another song
+         Song track3 = new Song("Ophelia", "The Band");							//Create another song
          this.albumTracks.add(track1);                                          //Add the first song to song list for the Band
          this.albumTracks.add(track2);                                          //Add the second song to song list for the Band 
          this.albumTracks.add(track2);                                          //Add the second song to song list for the Band 
